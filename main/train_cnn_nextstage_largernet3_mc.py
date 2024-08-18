ea483a96d1b816d36868bea729dec714654d1ab0
JohnLee <fantasy10final@gmail.com>
2024-08-18 13:16:18 +0800

    Random load states for mc

--- main/train_cnn_nextstage_largernet3_mc.py
+++ main/train_cnn_nextstage_largernet3_mc.py
@@ -37,7 +37,17 @@ def linear_schedule(initial_value, final_value=0.0):
 
 def make_env(seed=0):
     def _init():
-        env = SnakeEnv(seed=seed, length="random", is_grow=True)
+        state_name_list = [
+            'len350_state_2024_08_15_07_51_07.obj',
+            'len352_state_2024_08_15_07_46_41.obj',
+            'len353_state_2024_08_15_08_46_43.obj',
+            'len356_state_2024_08_15_08_59_36.obj',
+            'len358_state_2024_08_15_08_47_30.obj',
+            'len359_state_2024_08_15_09_00_32.obj',
+            'len366_state_2024_08_15_08_48_32.obj',
+            'len369_state_2024_08_15_08_49_35.obj'
+        ]
+        env = SnakeEnv(seed=seed, length="random", random_states=state_name_list, max_length=None, is_grow=True, silent_mode=True)
         env = ActionMasker(env, SnakeEnv.get_action_mask)
         env = Monitor(env)
         env.seed(seed)
