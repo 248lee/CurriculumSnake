0f3787e3d8d48d9cd87cab20d6b8940629737ef5
JohnLee <fantasy10final@gmail.com>
2024-09-05 18:04:01 +0800

    Control group

--- main/train_cnn_control_group.py
+++ main/train_cnn_control_group.py
@@ -7,6 +7,7 @@ from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.callbacks import CheckpointCallback
 
+from network_structures import Stage2CustomFeatureExtractorCNN
 from trmaskppo import TRMaskablePPO
 from sb3_contrib import MaskablePPO
 from sb3_contrib.common.wrappers import ActionMasker
@@ -18,7 +19,7 @@ if torch.backends.mps.is_available():
 else:
     NUM_ENV = 32
 LOG_DIR = "logs"
-ExperimentName = "snake_ob_len3_max130"
+ExperimentName = "snake_ob_BOSS_please_lower"
 
 os.makedirs(LOG_DIR, exist_ok=True)
 
@@ -35,9 +36,15 @@ def linear_schedule(initial_value, final_value=0.0):
 
     return scheduler
 
+
 def make_env(seed=0):
     def _init():
-        env = SnakeEnv(seed=seed, length=3, max_length=130, is_grow=True, silent_mode=True)
+        # Specify the directory
+        directory = "./game_states"
+
+        # Get the list of filenames in the specified directory
+        state_name_list = [filename for filename in os.listdir(directory) if os.path.isfile(os.path.join(directory, filename))]
+        env = SnakeEnv(seed=seed, length=state_name_list, formation='終焉', max_length=None, is_grow=True, silent_mode=True)
         env = ActionMasker(env, SnakeEnv.get_action_mask)
         env = Monitor(env)
         env.seed(seed)
@@ -75,32 +82,33 @@ def main():
         )
     else:
         lr_schedule = linear_schedule(2.5e-4, 2.5e-6)
-        clip_range_schedule = linear_schedule(0.15, 0.02)
+        clip_range_schedule = linear_schedule(0.16, 0.01)
         # clip_range_schedule = linear_schedule(0.150, 0.025)
         import torch as th
         from network_structures import CustomFeatureExtractorCNN
         policy_kwargs = dict(
-            features_extractor_class=CustomFeatureExtractorCNN,
+            features_extractor_class=Stage2CustomFeatureExtractorCNN,
             activation_fn=th.nn.ReLU,
-            net_arch=dict(pi=[256, 128], vf=[128])
+            net_arch=dict(pi=[512, 256, 128], vf=[256, 32])
         )
         # Instantiate a PPO agent using CUDA.
+        from gamma import gamma
         model = MaskablePPO(
             "CnnPolicy",
             env,
             device="cuda",
             verbose=1,
             n_steps=2048,
-            batch_size=512,
+            batch_size=4096,
             n_epochs=4,
-            gamma=0.94,
+            gamma=gamma,
             learning_rate=lr_schedule,
             clip_range=clip_range_schedule,
             tensorboard_log=LOG_DIR,
             policy_kwargs=policy_kwargs
         )
 
-        model.save('random_feature_extractor.zip')
+        # model.save('random_feature_extractor.zip')
 
     # Set the save directory
     if torch.backends.mps.is_available():
@@ -109,7 +117,7 @@ def main():
         save_dir = "trained_models_cnn"
     os.makedirs(save_dir, exist_ok=True)
 
-    checkpoint_interval = 15625 * 4 # checkpoint_interval * num_envs = total_steps_per_checkpoint
+    checkpoint_interval = 15625 * 32 # checkpoint_interval * num_envs = total_steps_per_checkpoint
     checkpoint_callback = CheckpointCallback(save_freq=checkpoint_interval, save_path=save_dir, name_prefix=ExperimentName)
 
     # Writing the training logs from stdout to a file
@@ -117,7 +125,7 @@ def main():
     log_file_path = os.path.join(save_dir, "training_log.txt")
 
     model.learn(
-        total_timesteps=int(100000000),
+        total_timesteps=int(200000000),
         callback=[checkpoint_callback],
         tb_log_name=ExperimentName,
         progress_bar=True
